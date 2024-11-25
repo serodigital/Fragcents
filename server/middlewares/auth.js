599d56134228e6b135190566e37248cfdac9fa8f
marolo <rachidims@gmail.com>
2024-11-25 20:55:29 +0200

    login functionality

---
+++ server/middlewares/auth.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const requireSignIn = (req,res,next) => {
+    try{
+        const decoded = jwt.verify(
+            req.headers.authorization, 
+            process.env.JWT_SECRET
+        );
+        // console.log("decoded => ", decoded);
+        req.user = decoded;
+        next();
+    }
+    catch(err){
+        return res.status(401).json(err);
+    }
+};
+
+export const isAdmin = async (req,res, next) => {
+    try{
+        const user = await User.findById(req.user._id);
+        if(user.role !== 1){
+            return res.status(401).send("Unauthorized");
+        }
+        else{
+            next();
+        }
+
+    }catch(err){
+        console.log(err);
+    }
+};
---
+++ server/routes/auth.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+//middlewares
+import { requireSignIn, isAdmin } from '../middlewares/auth.js';
+
+//controllers
+import {register, login, secret} from "../controllers/auth.js"
+
+router.post('/register', register);
+router.post('/login', login);
+//testing
+
+router.get('/secret', requireSignIn, isAdmin, secret);
+
+export default router;
