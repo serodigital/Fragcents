2c8af0eab5b0a01c58069170eaf7634163742b1a
MUDELY SYLVIA SAMBO <91378831+mudelysambo@users.noreply.github.com>
2024-12-04 23:00:46 +0200

    Insert to db

--- server/models/category.js
+++ server/models/category.js
@@ -8,6 +8,18 @@ const categorySchema = new mongoose.Schema({
         maxLength: 32,
         unique: true,
     },
+    class: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32,
+    },
+    price: {
+        type: Number,
+        trim: true,
+        required: true,
+        maxLength: 32,
+    },
     slug: {
         type: String,
         unique: true,
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,10 +6,12 @@ const router = express.Router();
 import { requireSignIn, isAdmin } from '../middlewares/auth.js';
 
 //controllers
-import {register, login, secret} from "../controllers/auth.js"
+import {register, login, secret} from "../controllers/auth.js";
+import { createCategory } from '../controllers/product_Catergory.js';
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/category', createCategory)
 //testing
 
 router.get('/secret', requireSignIn, isAdmin, secret);
