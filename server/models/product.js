2b057f7f83b2e7e60adfedfaf05e1a4a77d53e3c
marolo <rachidims@gmail.com>
2024-12-09 20:18:35 +0200

    product schema update

--- server/models/product.js
+++ server/models/product.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+const { ObjectId} = mongoose.Schema;
 
 const productSchema = new mongoose.Schema({
     name: {
@@ -10,10 +11,8 @@ const productSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        trim: true,
         required: true,
-        maxLength: 32,
-        unique: true,
+        maxLength: 2000,
     },
     gender: {
         type: String,
@@ -24,9 +23,7 @@ const productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true,
-        min: 2,
-        
-        
+        min: 2,  
     },
     AvailableInBulk: {
         type: Boolean,
@@ -37,7 +34,34 @@ const productSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
     },
-});
+    // Category will link with Category via the Object Id
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true,
+    },
+    // To manage inventory
+    quantity: {
+        type: Number,     
+    },
+    //show how many have been sold
+    sold: {
+        type: Number,
+        default: 0,
+    },
+    //save photo as binary in mango DB , lets consider s3 bucket
+    photo: {
+        data: Buffer,
+        contentType: String,
+    },
+    //if shipping is required
+    shipping: {
+        required: false,
+        type: Boolean
+    },
+  },
+  { timestamps: true } // adding a time stamp
+);
 
 export default mongoose.model('Product', productSchema);
 //creates a 'Collection' in the db called (Product)
